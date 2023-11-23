4a06853a95cea6d341888070fd3122155ebd19a0
Timur010 <duakar010@gmail.com>
2023-11-23 18:03:24 +0300

    добавил аутентификацию и добавления фото профиля

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const User = require('../models/user');
+const multer = require('multer');
+const sharp = require('sharp');
+const auth = require('../middleware/auth')
+
 
 const router = new express.Router();
+
+//helpers 
+
+const upload = multer({
+    limits: {
+        fieldSize: 100000000
+    }
+})
 
 // creat a new user
 router.post('/users', async (req, res) => {
@@ -40,6 +52,19 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+// search users by name
+router.get('/user/search', async (req, res) => {
+    try {
+        const name = req.query.name
+        const users = await User.find({name: {$regex: name, $options: 'i' }})
+        res.send(users)
+    }
+    catch(e) {
+        res.status(500).send(e)
+    }
+} )
+
+
 // delete User Route
 
 router.delete('/user/:id', async (req, res) => {
@@ -75,6 +100,24 @@ router.get('/user/:id', async (req, res) => {
     catch(e) {
         res.status(500).send(e)
     }
+})
+
+//Post User Profile imge
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250}).png().toBuffer()
+    if (req.user.avatar != null) {
+        req.user.avatar = null
+        req.user.avatarExists = false
+    }
+
+    req.user.avatar = buffer
+    req.user.avatarExists = true
+    await req.user.save()
+
+    res.send(buffer)
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
 
 module.exports = router
